2abcd8b8b42216d3d9b898583b049e4afc27b5a6
OtnielPerez <otnielperez05@gmail.com>
2020-03-21 10:02:29 -0400

    CRUD en .NET Core

---
+++ CRUD/CRUD/Data/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD.Models;
+
+namespace CRUD.Data
+{
+    public class DbInitializer
+    {
+        //Comprobar si existen categorias
+        public static void Inicializar (CRUDContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[]
+            {
+                new Categoria{Nombre="Programacion (:", Descripcion="Encontraras algo interensante para ver en cuarentena", Estado=true}
+            };
+            foreach(Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ CRUD/CRUD/Models/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CRUD.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required (ErrorMessage = "Campo requerido")]
+        [StringLength(50,MinimumLength =3, ErrorMessage ="Mínimo 3 maximo 50 caracteres")]
+        public String Nombre { get; set; }
+        [StringLength(256, ErrorMessage ="La descripcion no puede tener mas de 256 caracteres ):")]
+        [Display(Name = "Descripción")]
+        public String Descripcion { get; set; }
+        public Boolean Estado { get; set; }
+        
+        
+
+    }
+}
